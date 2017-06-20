7f46ed41340135609e42466f8eaf8073be868091
User <User@DESKTOP-GNESREK>
2017-06-20 14:06:18 +0200

    It is possible to switch between the tabs (no recognized errors)

---
+++ Script/IndexScript.js
@@ -0,0 +1,18 @@
+function openCity(evt, tab) {
+	var tablinks;
+
+	tablinks = document.getElementsByClassName("tablinks active");
+	tablinks[0].className ="tablinks";
+
+	tabContent = document.getElementById("tabcontent");
+	tabContent.src = tab + ".html";
+
+
+
+	evt.currentTarget.className += " active";
+} 
+
+
+/* 
+ * 
+ */
