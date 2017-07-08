c41d8cd45634b2c202b31953858a23f6434e771d
User <User@DESKTOP-GNESREK>
2017-07-08 22:57:24 +0200

    trying new ideas

--- Script/IndexScript.js
+++ Script/IndexScript.js
@@ -1,18 +1,14 @@
-function openCity(evt, tab) {
+function changeTabs(evt, tab) {
 	var tablinks;
-
 	tablinks = document.getElementsByClassName("tablinks active");
 	tablinks[0].className ="tablinks";
-
 	tabContent = document.getElementById("tabcontent");
 	tabContent.src = tab + ".html";
-
-
-
 	evt.currentTarget.className += " active";
+	
 } 
 
-
-/* 
- * 
- */
+function rollOut(evt) {
+	
+	
+}	
