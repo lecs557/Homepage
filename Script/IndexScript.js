ee15d617f457e2bb6e5db8f79abd0deed4ff5a9e
User <User@DESKTOP-GNESREK>
2017-07-30 23:09:01 +0200

    code is working :) should be written in a better way in the next time ;)

--- Script/IndexScript.js
+++ Script/IndexScript.js
@@ -1,5 +1,14 @@
+var id;	
+var rolleOut ;
+var wid;
+
+function init(){
+	rolleOut = document.getElementById("rollOut");
+}
+
+
 function changeTabs(evt, tab) {
-	var tablinks;
+	var tablinks, tabContent;
 	tablinks = document.getElementsByClassName("tablinks active");
 	tablinks[0].className ="tablinks";
 	tabContent = document.getElementById("tabcontent");
@@ -9,6 +18,36 @@ function changeTabs(evt, tab) {
 } 
 
 function rollOut(evt) {
-	
+	clearInterval(id);
+	rolleOut.style.width = "0px";
+	rolleOut.style.left = evt*200 +"px";
+	roll(false);
 	
 }	
+
+function reset() {
+	r = true;
+	clearInterval(id);
+	rolleOut.onmouseover = function(){clearInterval(id); r = false; roll(r);};
+	rolleOut.onmouseout  = function(){clearInterval(id); r = true; roll(r);};
+	roll(r);
+}
+
+function roll(bln) {	
+	wid = parseInt(rolleOut.style.width);
+	id = setInterval(frame, 1);
+	function frame() {
+		if (rolleOut.style.width == "0px" && bln){
+			rolleOut.hidden = true; clearInterval(id);}
+		else
+			rolleOut.hidden = false;
+		if (bln && wid > 0) {
+			wid = wid-10;
+			rolleOut.style.width = wid + "px";		
+		} else if (wid < 500) {
+			wid=wid+10;
+			rolleOut.style.width = wid + "px";
+		}
+	}
+	
+}
